f696e9f3a60a3e424212b5a668db3319d1bf1b69
byshiny <bs5ht@virginia.edu>
2018-12-29 14:27:43 -0500

    fetch exercises done

--- gopl/ch1/fetch/main.go
+++ gopl/ch1/fetch/main.go
@@ -9,25 +9,36 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Println("herro")
 	for _, url := range os.Args[1:] {
+		hasPrefix := strings.HasPrefix(url, "http://")
+		if !hasPrefix {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
+		} else {
+			fmt.Println(err)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		statusReader := strings.NewReader(resp.Status)
+		_, err = io.Copy(os.Stdout, statusReader)
+		// b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		// 	os.Exit(1)
+		// }
+		//fmt.Printf("%s", b)
 	}
 }
 
