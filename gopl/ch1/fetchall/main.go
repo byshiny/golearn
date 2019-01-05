d058c2504d9f8a4476a94e09982146d9d87d95f8
byshiny <bs5ht@virginia.edu>
2019-01-05 04:08:25 -0500

    fetched the top 1 mill websites

--- gopl/ch1/fetchall/main.go
+++ gopl/ch1/fetchall/main.go
@@ -8,24 +8,58 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"path"
+	"strings"
 	"time"
 )
 
 func main() {
+	topWebsites := readCsvFile("top-1m.csv")
+
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
+	// os.Args[1:]
+	for _, websiteRecord := range topWebsites {
+		url := websiteRecord[1]
 		go fetch(url, ch) // start a goroutine
 	}
-	for range os.Args[1:] {
+	for range topWebsites {
 		fmt.Println(<-ch) // receive from channel ch
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func readCsvFile(filePath string) [][2]string {
+	// Load a csv file.
+	f, _ := os.Open(filePath)
+	recordList := make([][2]string, 100)
+	// Create a new reader.
+	r := csv.NewReader(bufio.NewReader(f))
+	for {
+		record, err := r.Read()
+		// Stop at EOF.
+		if err == io.EOF {
+			break
+		}
+		// Display record.
+		// ... Display record length.
+		// ... Display all individual elements of the slice.
+		//fmt.Println(record)
+		// for value := range record {
+		// 	fmt.Printf("  %v\n", record[value])
+		// }
+		websiteLink := "https://www." + record[1]
+		recordList = append(recordList, [2]string{record[0], websiteLink})
+	}
+
+	return recordList
 }
 
 func fetch(url string, ch chan<- string) {
@@ -36,12 +70,24 @@ func fetch(url string, ch chan<- string) {
 		return
 	}
 
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	//watch out for https vs http - need to take care of http later.
+	//this is because when you write to the file it doesn't like slashes
+	httpStrippedFilename := strings.Replace(url, "https://", "", 1)
+	urlname := httpStrippedFilename + ".html"
+	directoryName := "./websites"
+	fullPath := path.Join(directoryName, urlname)
+	fmt.Println(fullPath)
+	to, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nbytes, err := io.Copy(to, resp.Body)
 	resp.Body.Close() // don't leak resources
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
 }
