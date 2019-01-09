f2874bfbb44c900a5023864f6cf82f862b82d985
byshiny <bs5ht@virginia.edu>
2019-01-09 11:23:57 -0500

    color exercise done

--- gopl/ch3/surface/main.go
+++ gopl/ch3/surface/main.go
@@ -6,12 +6,21 @@
 
 // Surface computes an SVG rendering of a 3-D surface function.
 // Need to pipe this as an html file to see the actual picture ./surface > surface.xml
+
+//Byung's notes.
+//skipped exercise #2 because I'd have to do  more research on the math to make these weird shapes?
 package main
 
 import (
 	"fmt"
 	"math"
 )
+
+type point struct {
+	x float64
+	y float64
+	z float64
+}
 
 const (
 	width, height = 600, 320            // canvas size in pixels
@@ -30,18 +39,49 @@ func main() {
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, az := corner(i+1, j)
+			bx, by, bz := corner(i, j)
+			cx, cy, cz := corner(i, j+1)
+			dx, dy, dz := corner(i+1, j+1)
+
+			ptA := point{ax, ay, az}
+			ptB := point{bx, by, bz}
+			ptC := point{cx, cy, cz}
+			ptD := point{dx, dy, dz}
+			ptList := []point{ptA, ptB, ptC, ptD}
+			ctrPt := findCentroid(&ptList)
+
+			fmt.Println("%g, %g, %g, %g", az, bz, cz, dz)
 			if areParametersValid(ax, ay, bx, by, cx, cy, dx, dy) {
-				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-					ax, ay, bx, by, cx, cy, dx, dy)
+				if ctrPt.z < 0 {
+					fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' stroke='blue'/>\n",
+						ax, ay, bx, by, cx, cy, dx, dy)
+				} else {
+					fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' stroke='red'/>\n",
+						ax, ay, bx, by, cx, cy, dx, dy)
+				}
 			}
 
 		}
 	}
 	fmt.Println("</svg>")
+}
+
+func findCentroid(ptListPtr *[]point) point {
+	var ctrX float64
+	var ctrY float64
+	var ctrZ float64
+	var ptCtr float64
+	ptList := *ptListPtr
+	for _, v := range ptList {
+		ctrX += v.x
+		ctrY += v.y
+		ctrZ += v.z
+		ptCtr++
+	}
+
+	ctrPt := point{ctrX / ptCtr, ctrY / ptCtr, ctrZ / ptCtr}
+	return ctrPt
 }
 
 func areParametersValid(ax float64, ay float64, bx float64, by float64, cx float64, cy float64, dx float64, dy float64) bool {
@@ -73,7 +113,7 @@ func areParametersValid(ax float64, ay float64, bx float64, by float64, cx float
 	return true
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, float64) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
@@ -84,7 +124,7 @@ func corner(i, j int) (float64, float64) {
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, z
 }
 
 func f(x, y float64) float64 {
